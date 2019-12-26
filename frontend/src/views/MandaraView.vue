3242f1d63da48df45d187fbb6c8242a6c6695d36
dyjung123 <jeongdongryun@gmail.com>
2019-12-27 00:16:37 +0900

    feat: add MandaraView

---
+++ frontend/src/views/MandaraView.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="mandara">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm border">
+          One of three columns
+        </div>
+        <div class="col-sm border">
+          One of three columns
+        </div>
+        <div class="col-sm border">
+          One of three columns
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm border">
+          One of three columns
+        </div>
+        <div class="col-sm border">
+          One of three columns
+        </div>
+        <div class="col-sm border">
+          One of three columns
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm border">
+          One of three columns
+        </div>
+        <div class="col-sm border">
+          One of three columns
+        </div>
+        <div class="col-sm border">
+          One of three columns
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  Vue,
+  Component,
+} from 'vue-property-decorator';
+
+@Component
+export default class MandaraView extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+  .mandara {
+  }
+
+  .border {
+    border: 1px solid #333333;
+  }
+</style>
